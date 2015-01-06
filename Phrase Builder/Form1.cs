bcff327928033f6fe60c0bd8179ee3acf610a232
OliPicard <oliver@ufomail.co.uk>
2015-01-06 21:38:42 +0000

    +Charlie Sheen #Winning

--- Phrase Builder/Form1.cs
+++ Phrase Builder/Form1.cs
@@ -23,10 +23,10 @@ namespace Phrase_Builder
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String[] NameListOne = new string[] { "Cosmic Panda", "Pizza Delivery Boy", "Boom Goes the Dinimatie","why is the rum gone?","Grumpy Cat","Serious Octopus","Alt+ F4","Tasty Pickle","NoNoNoNo Cat","Oh Long Johnson","Chocolate Rain"};
+            String[] NameListOne = new string[] { "Cosmic Panda", "Pizza Delivery Boy", "Boom Goes the Dinimatie","why is the rum gone?","Grumpy Cat","Serious Octopus","Alt+ F4","Tasty Pickle","NoNoNoNo Cat","Oh Long Johnson","Chocolate Rain","Charlie Sheen"};
             Random pepperpig = new Random(); //create a new random.
             int length = NameListOne.Length; //get the length of the string
-            int rand1 = (int)(pepperpig.Next(0,11)); //provide pepperpig(random) guidence.
+            int rand1 = (int)(pepperpig.Next(0,12)); //provide pepperpig(random) guidence.
             label2.Text = (NameListOne[rand1]);//issue command to list said item.
         }
 
